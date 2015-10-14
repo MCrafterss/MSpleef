9449a61e46268efe5d3798b4b85663152a8a2b90
Taha_The_Hacker <tahasalah585@gmail.com>
2015-10-14 21:33:43 +0300

    Update GameEnd.php

--- src/TahaTheHacker/MSpleef/GameEnd.php
+++ src/TahaTheHacker/MSpleef/GameEnd.php
@@ -19,6 +19,7 @@ class GameEnd extends PluginTask{
 
   public function onRun($tick){
 	$this->plugin->seconds++;
+	$level = $this->plugin->getServer()->getLevelByName($this->yml["spleef-world"]);
     if($this->plugin->seconds === 120){
     for($x = $this->plugin->yml["spleef-Min-floor-X"]; $x <= $this->plugin->yml["spleef-Max-floor-X"]; $x++):
     for($y = $this->plugin->yml["spleef-Min-floor-Y"]; $y <= $this->plugin->yml["spleef-Max-floor-Y"]; $y++):
