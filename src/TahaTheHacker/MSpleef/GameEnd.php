e7859c9056b61c4dd34c885ef951d4072ac2d87d
Taha_The_Hacker <tahasalah585@gmail.com>
2015-10-16 21:08:40 +0300

    Update GameEnd.php

--- src/TahaTheHacker/MSpleef/GameEnd.php
+++ src/TahaTheHacker/MSpleef/GameEnd.php
@@ -22,20 +22,24 @@ class GameEnd extends PluginTask{
   }
 
   public function onRun($tick){
-	$this->plugin->seconds++;
-	$level = $this->plugin->getServer()->getLevelByName($this->plugin->yml["spleef-world"]);
-    if($this->plugin->seconds === $this->plugin->yml["spleef-time"]){
+  $this->plugin->seconds++;
+
+  $level = $this->plugin->getServer()->getLevelByName($this->plugin->yml["spleef-world"]);
+
+      if($this->plugin->seconds === $this->plugin->yml["spleef-time"]){
+
     for($x = $this->plugin->yml["spleef-Min-floor-X"]; $x <= $this->plugin->yml["spleef-Max-floor-X"]; $x++){
     for($y = $this->plugin->yml["spleef-Min-floor-Y"]; $y <= $this->plugin->yml["spleef-Max-floor-Y"]; $y++){
     for($z = $this->plugin->yml["spleef-Min-floor-Z"]; $z <= $this->plugin->yml["spleef-Max-floor-X"]; $z++){
         $level->setBlock(new Vector3($x, $y, $z), Block::get(7,0));
-      $this->plugin->getServer()->getScheduler()->cancelTask($this->plugin->gameStartTask);
     }
     }
     }
       $this->plugin->gameStarted = false;
-      $this->plugin->getServer()->broadcastMessage("Spleef Game Ended!");
       $this->plugin->seconds = 0;
+      $this->plugin->getServer()->broadcastMessage("Spleef Game Ended!");
+      $this->plugin->getServer()->getScheduler()->cancelTask($this->plugin->gameEndTask);
+
   }//If Seconds.
   }//onRun
 
