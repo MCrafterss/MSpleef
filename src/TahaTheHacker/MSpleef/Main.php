7dcb15abebc495f98e9bc97076e96eea78bc879d
Taha_The_Hacker <tahasalah585@gmail.com>
2015-10-16 21:12:27 +0300

    Update Main.php

--- src/TahaTheHacker/MSpleef/Main.php
+++ src/TahaTheHacker/MSpleef/Main.php
@@ -26,7 +26,7 @@ use pocketmine\network\protocol\UpdateBlockPacket;
 
   class Main extends PluginBase implements Listener {
 
-    public $gameStartTask;// i will use it
+    public $gameEndTask;// i will use it
     public $seconds = 0;//Yes
    //Config files
     public $items;
@@ -37,21 +37,21 @@ use pocketmine\network\protocol\UpdateBlockPacket;
 
   public function onEnable(){
      //Initializing config files
-      $this->saveDefaultConfig();
+      $this->saveResource("config.yml");
       $this->saveResource("Items.yml");
       $this->saveResource("rewards.yml");
-      $items = new Config($this->getDataFolder()."Items.yml",Config::YAML);
+      $items = new Config($this->getDataFolder() . "Items.yml", Config::YAML);
       $this->items = $items->getAll();
-      $rewards = new Config($this->getDataFolder()."rewards.yml",Config::YAML);
+      $rewards = new Config($this->getDataFolder() . "rewards.yml", Config::YAML);
       $this->rewards = $rewards->getAll();
-      $yml = new Config($this->getDataFolder()."config.yml",Config::YAML);
+      $yml = new Config($this->getDataFolder() . "config.yml", Config::YAML);
       $this->yml = $yml->getAll();
-	$this->getServer()->getLogger()->debug("Config files have been saved!");
+  $this->getServer()->getLogger()->debug("Config files have been saved!");
 
-   		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+      $this->getServer()->getPluginManager()->registerEvents($this, $this);
 
-   		$this->getServer()->getLogger()->info(TextFormat::BOLD.TextFormat::GOLD."M".TextFormat::AQUA."Spleef ".TextFormat::GREEN."Enabled".TextFormat::RED."!");
-   		
+      $this->getServer()->getLogger()->info(TextFormat::BOLD . TextFormat::GOLD . "M" . TextFormat::AQUA . "Spleef " . TextFormat::GREEN . "Enabled" . TextFormat::RED . "!");
+      
   }//onEnable
   
   public function gameStart(){
@@ -60,12 +60,12 @@ use pocketmine\network\protocol\UpdateBlockPacket;
     for($x = $this->yml["spleef-Min-floor-X"]; $x <= $this->yml["spleef-Max-floor-X"]; $x++){
     for($y = $this->yml["spleef-Min-floor-Y"]; $y <= $this->yml["spleef-Max-floor-Y"]; $y++){
     for($z = $this->yml["spleef-Min-floor-Z"]; $z <= $this->yml["spleef-Max-floor-X"]; $z++){
-    	    $level->setBlock(new Vector3($x, $y, $z), Block::get(0,0));//prevents from a client-side issue when breaking the snow it becomes the last block it changed. (bedrock) so then players won't fall.
+          $level->setBlock(new Vector3($x, $y, $z), Block::get(0));//prevents from a client-side issue when breaking the snow it becomes the last block it changed. (bedrock) so then players won't fall.
             $level->setBlock(new Vector3($x, $y, $z), Block::get($this->yml["spleef-floor-reset-block-ID"],$this->yml["spleef-floor-reset-block-damage"]));
           }
           }
           }
-          $this->gameStartTask = $this->getServer()->getScheduler()->scheduleRepeatingTask(new GameEnd($this), 20)->getTaskId();
+          $this->gameEndTask = $this->getServer()->getScheduler()->scheduleRepeatingTask(new GameEnd($this), 20)->getTaskId();
           $this->getServer()->broadcastMessage("Spleef Game Started!");
   }//GameStart
 
@@ -74,12 +74,19 @@ use pocketmine\network\protocol\UpdateBlockPacket;
       case "ms":
       if (isset($args[0])){
     switch($args[0]){
+
       case "start":
       $this->gameStart();
+      return true;
+      break;
+
       case "stop":
       if($this->gameStarted === true){
       $this->gameStarted = false;
     }//If
+    return true;
+    break;
+    
     }//switch 2
       }//isset
     }//switch1
@@ -88,9 +95,9 @@ use pocketmine\network\protocol\UpdateBlockPacket;
   
  public function onInteract(PlayerInteractEvent $event){
     if($event->getBlock()->getX() === $this->yml["spleef-start-block-X"] && $event->getBlock()->getY() === $this->yml["spleef-start-block-Y"] && $event->getBlock()->getZ() === $this->yml["spleef-start-block-Z"]){
-    	if($this->gameStarted === false){
-    		$this->gameStart();
-    	}
+      if($this->gameStarted === false){
+        $this->gameStart();
+      }
     }//if1
  }//onInteract
   }//Main
