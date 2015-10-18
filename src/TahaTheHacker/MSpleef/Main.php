240aa95b050d82c02521b7374c720ab213bc0c05
Taha_The_Hacker <tahasalah585@gmail.com>
2015-10-18 13:43:49 +0300

    Working on a new feature...

--- src/TahaTheHacker/MSpleef/Main.php
+++ src/TahaTheHacker/MSpleef/Main.php
@@ -3,28 +3,33 @@
 namespace TahaTheHacker\MSpleef;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\utils\TextFormat;
+use pocketmine\utils\TextFormat as Color;
 use pocketmine\utils\Config;
 use pocketmine\item\item;
 use pocketmine\event\Event;
 use pocketmine\level\Position;
 use pocketmine\event\Listener;
+
 use pocketmine\command\Command;
 use pocketmine\command\CommandExecutor;
 use pocketmine\command\CommandSender;
 use pocketmine\command\PluginCommand;
 use pocketmine\command\ConsoleCommandSender;
+
 use pocketmine\Server;
 use pocketmine\level\Level;
 use pocketmine\scheduler\ServerScheduler;
 use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\math\Vector3;
 use pocketmine\block\Block;
 use pocketmine\item\ItemBlock;
 use pocketmine\event\block\BlockEvent;
 use pocketmine\network\protocol\UpdateBlockPacket;
 
+
   class Main extends PluginBase implements Listener {
+
    //Tasks
     public $gameEndTask;// i will use it
     public $seconds = 0;//Yes
@@ -34,7 +39,6 @@ use pocketmine\network\protocol\UpdateBlockPacket;
     public $reward;
     public $yml;
     public $level;
-    
    //Game
     public $gameStarted = false;
 
@@ -52,24 +56,25 @@ use pocketmine\network\protocol\UpdateBlockPacket;
       $yml = new Config($this->getDataFolder() . "config.yml", Config::YAML);
       $this->yml = $yml->getAll();
       
-  $this->getServer()->getLogger()->debug("Config files have been saved!");
+  $this->getLogger()->debug("Config files have been saved!");
 
       $this->getServer()->getPluginManager()->registerEvents($this, $this);
 
-      $this->getServer()->getLogger()->info(TextFormat::BOLD . TextFormat::GOLD . "M" . TextFormat::AQUA . "Spleef " . TextFormat::GREEN . "Enabled" . TextFormat::RED . "!");
+      $this->getServer()->getLogger()->info(Color::BOLD . Color::GOLD . "M" . Color::AQUA . "Spleef " . Color::GREEN . "Enabled" . Color::RED . "!");
       
   }//onEnable
   
   public function gameStart(){
-    $this->gameStarted = true;
-    $this->level = $this->getServer()->getLevelByName($this->yml["spleef-world"]);
-    $level = $this->getServer()->getLevelByName($this->yml["spleef-world"]);
-    
+        $level = $this->getServer()->getLevelByName($this->yml["spleef-world"]);
       for($x = $this->yml["spleef-Min-floor-X"]; $x <= $this->yml["spleef-Max-floor-X"]; $x++){
       for($y = $this->yml["spleef-Min-floor-Y"]; $y <= $this->yml["spleef-Max-floor-Y"]; $y++){
       for($z = $this->yml["spleef-Min-floor-Z"]; $z <= $this->yml["spleef-Max-floor-X"]; $z++){
-      
-            $level->setBlock(new Vector3($x, $y, $z), Block::get(0));//prevents from a client-side issue when breaking the snow it becomes the last block it changed. (bedrock) so then players won't fall.
+    $level->setBlock(new Vector3($x, $y, $z), Block::get(0));//prevents from a client-side issue when breaking the snow it becomes the last block it changed. (bedrock) so then players won't fall.
+
+    $this->level = $this->getServer()->getLevelByName($this->yml["spleef-world"]);
+    
+
+    
             $level->setBlock(new Vector3($x, $y, $z), Block::get($this->yml["spleef-floor-reset-block-ID"],$this->yml["spleef-floor-reset-block-damage"]));
           }
           }
@@ -81,6 +86,7 @@ use pocketmine\network\protocol\UpdateBlockPacket;
           $this->getServer()->broadcastMessage($msg);
           
           }
+        $this->gameStarted = true;
   }//GameStart
 
   public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
@@ -95,7 +101,7 @@ use pocketmine\network\protocol\UpdateBlockPacket;
       break;
 
       case "stop":
-      if($this->gameStarted === true){
+      if($this->gameStarted == true){
         for($x = $this->yml["spleef-Min-floor-X"]; $x <= $this->yml["spleef-Max-floor-X"]; $x++){
         for($y = $this->yml["spleef-Min-floor-Y"]; $y <= $this->yml["spleef-Max-floor-Y"]; $y++){
         for($z = $this->yml["spleef-Min-floor-Z"]; $z <= $this->yml["spleef-Max-floor-X"]; $z++){
@@ -108,6 +114,12 @@ use pocketmine\network\protocol\UpdateBlockPacket;
     }//If
     return true;
     break;
+      case "reload":
+      $plugin = $this->getServer()->getPluginManager()->getPlugin("MSpleef");
+      $this->getServer()->getPluginManager()->disablePlugin($plugin);
+      $this->getServer()->getPluginManager()->enablePlugin($plugin);
+      return true;
+      break;
     
     }//switch 2
       } else { $sender->sendMessage("Usage: /ms <start/stop>"); }//isset
@@ -115,11 +127,26 @@ use pocketmine\network\protocol\UpdateBlockPacket;
 
   }//onCommand
   
- public function onInteract(PlayerInteractEvent $event){
+ public function gameStartButton(PlayerInteractEvent $event){
     if($event->getBlock()->getX() === $this->yml["spleef-start-block-X"] && $event->getBlock()->getY() === $this->yml["spleef-start-block-Y"] && $event->getBlock()->getZ() === $this->yml["spleef-start-block-Z"]){
       if($this->gameStarted === false){
         $this->gameStart();
       } else { $event->getPlayer()->sendMessage("Spleef game already started!"); }
     }//if1
- }//onInteract
+ }//gameStartButton
+
+ public function spleefItems(PlayerMoveEvent $event){
+  $player = $event->getPlayer();
+    foreach($this->items["Items"] as $i){
+      for($x = $this->yml["spleef-Min-floor-X"]; $x <= $this->yml["spleef-Max-floor-X"]; $x++){
+      for($y = $this->yml["spleef-Min-floor-Y"]; $y <= $this->yml["spleef-Max-floor-Y"]; $y++){
+      for($z = $this->yml["spleef-Min-floor-Z"]; $z <= $this->yml["spleef-Max-floor-X"]; $z++){
+   if(round($event->getFrom()->getX()) == round($x) && round($event->getFrom()->getY())-1 == round($y) && round($event->getFrom()->getZ()) == round($z)){
+    $player->getInventory()->addItem(Item::get($i["id"], $i["damage"]));
+    } elseif(round($event->getFrom()->getX()) == $x && round($event->getFrom()->getY())-2 == $y && round($event->getFrom()->getZ()) == $z){ $player->getInventory()->addItem(Item::get($i["id"], $i["damage"])); } else { $player->getInventory()->removeItem(Item::get($i["id"], $i["damage"])); }//if
+  }//3
+  }//2
+  }//1
+  }//foreach
+ }//spleefItems
   }//Main
